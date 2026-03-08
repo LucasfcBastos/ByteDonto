f85c0e802f8dd7afecf5774ee4d5e64d86c7c6c8
Lucas LBF Furango <lucas.software.engineering@gmail.com>
2026-03-08 12:03:49 -0300

    🎨 - I added responsiveness and configured it for the new screen. furango_lbf <lucas.software.engineering@gmail.com> Descrição: I added responsiveness and configured it for the new screen.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,8 +5,9 @@ import LandingPage from "../pages/general/LandingPage";
 import LoginPage from "../pages/general/LoginPage";
 import CadastroPage from "../pages/general/CadastroPage";
 
+import ListClinic from "../pages/owner/ListClinic";
 import ViewClinic from "../pages/owner/ViewClinic";
-import RegisterClinic from "../pages/owner/RegisterClinic.jsx";
+import RegisterClinic from "../pages/owner/RegisterClinic";
 
 import DashboardPage from "../pages/dashboard/DashboardPage";
 
@@ -29,8 +30,9 @@ export function AppRoutes() {
           <Route path="/cadastro" element={<CadastroPage />} />
 
           {/* ROTAS QUE VÃO SER PRIVADAS E SÓ ENTRA AUTHNTICADO COM TOKEN */}
-          <Route path="/owner/clinic" element={<ViewClinic />} />
+          <Route path="/owner/clinic" element={<ListClinic />} />
           <Route path="/owner/clinic/register" element={<RegisterClinic />} />
+          <Route path="/owner/view/clinic" element={<ViewClinic />} />
 
           {/* Private routes */}
           <Route path="/dashboard" element={
